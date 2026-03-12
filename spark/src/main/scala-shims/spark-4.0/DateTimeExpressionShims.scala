bcb7d679bd5b864cacd0617274220368f2bf084e
Cuong Nguyen <ctring23@gmail.com>
2026-03-12 20:21:26 +0000

    [ES-1684994][LC-12311] Fix timestamp overflow in dataskipping

--- spark/src/main/scala-shims/spark-4.0/DateTimeExpressionShims.scala
+++ spark/src/main/scala-shims/spark-4.0/DateTimeExpressionShims.scala
@@ -26,7 +26,8 @@ object DateTimeExpressionShims {
     expr match {
       case _: AddMonthsBase | _: DateAdd | _: DateAddInterval | _: DateDiff | _: DateSub |
            _: DatetimeSub | _: LastDay | _: MonthsBetween | _: NextDay | _: SubtractDates |
-           _: SubtractTimestamps | _: TimeAdd | _: TimestampAdd | _: TimestampAddYMInterval |
+           _: SubtractTimestamps | _: TimestampAddInterval | _: TimestampAdd |
+           _: TimestampAddYMInterval |
            _: TimestampDiff | _: TruncInstant => true
       case _ => false
     }
